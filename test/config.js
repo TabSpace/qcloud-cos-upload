fdd6982ec3523e0e4e0b7c15584cfa4d28fc8f7c
TabSpace <pillar0514@163.com>
2019-02-19 09:38:38 +0800

    fix: 测试CI流程

--- test/config.js
+++ test/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	AppId: '1256085844',
-	SecretId: 'AKIDrWwCDzW3HLAkCVLaPwEI6mDONApGQWNz',
+	SecretId: 'AKIDrWwCDzW3HLAkCVLaPwEI6mDONApGQWNz-',
 	SecretKey: 'b040hDVcU6Vinc2D9qIUYUBgy5xgCXkj',
 	Bucket: 'cosup',
 	Region: 'ap-chengdu'
